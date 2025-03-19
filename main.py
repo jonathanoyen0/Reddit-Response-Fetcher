9402c0d5a81ab7a9acec5a2e900fbd1d9b379997
jonathanoyen0 <149101224+jonathanoyen0@users.noreply.github.com>
2025-03-19 10:29:51 -0500

    Made changes to post id scraper

--- main.py
+++ main.py
@@ -1,27 +1,21 @@
 import requests
-
+import pandas as pd
 from dotenv import load_dotenv
-import os
 
-load_dotenv()  # Load environment variables from .env
-
-CLIENT_ID = os.getenv("CLIENT_ID")
-CLIENT_SECRET = os.getenv("CLIENT_SECRET")
-USER_AGENT = os.getenv("USER_AGENT")
-
-def get_post():
-    url = ""
-    #This will attempt to get the random post from reddit
+def get_random_post(subreddit: str):
+    url = f"https://www.reddit.com/r/{subreddit}/random.json"
     try:
         response = requests.get(url)
         if response.status_code == 200:
             print("We did it")
             response = response.json()
-            return response
+            response
         else:
             print("Error: ", response.status_code)
             return None
-    #This just cathches any network related errors
     except requests.exceptions.RequestException as e:
         print("Error: ", e)
         return None
+    
+df = pd.read_csv("subreddits_public.csv",names=["base10_id","base36_reddit_id","creation_epoch","subreddit_name","subscribers_count"])
+
