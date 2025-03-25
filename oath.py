a8b8167056cd831f8749baf4823f9c4e05599203
jonathanoyen0 <149101224+jonathanoyen0@users.noreply.github.com>
2025-03-25 11:51:36 -0500

    added a file to store the last token retrived to reduce unnecessary retrival of a new token

--- oath.py
+++ oath.py
@@ -1,8 +1,22 @@
 import json
 import requests
+import time
+
+#checks if the currents token is within its life time, to prevent unnecassary retrevial of tokens from API
+def check_current_token(token_file):
+    if time.time() < token_file["time_created"] + token_file["lifetime"] + 60:
+        return True
+    else:
+        return False
 
 #Uses a file named 'pw.json' and to store the values 
 def get_oath_token():
+    #Checking to see if we even need token
+    token_file = json.load(open("oath_token.json", 'r+'))
+    if (check_current_token(token_file=token_file)):
+        return token_file["token"]
+    
+    #getting token if we need it
     secrets = json.load(open("secrets.json"))
     auth = requests.auth.HTTPBasicAuth(secrets["CLIENT_ID"], secrets["CLIENT_SECRET"])
     data = json.load(open("pw.json", 'r'))
@@ -12,6 +26,10 @@ def get_oath_token():
         if(response.status_code == 200):
             print("Got oath token")
             response = response.json()
+            token_file["token"] = response["access_token"]
+            token_file["time_created"] = time.time()
+            token_file["lifetime"] = response["expires_in"]
+            json.dump(token_file, open("oath_token.json", 'w'), indent=4)
             return response
         else:
             print("Error: ", response.status_code)
@@ -19,3 +37,5 @@ def get_oath_token():
     except requests.exceptions.RequestException as e:
         print("Error: ", e)
         return None
+
+get_oath_token()
--- random_post.py
+++ random_post.py
@@ -1,19 +1,18 @@
 import requests
-from oath import get_oath_token
 import json
 
+#returns the newest reddit post on a given subreddit, returns response from api in json
 def get_random_post(subreddit: str):
     url = f"https://www.reddit.com/r/{subreddit}/new.json"
+    token_file = json.load(open("oath_token.json", 'r'))
     headers = {
-        "Authorization": get_oath_token()["access_token"],
+        "Authorization": token_file["token"],
         "User-Agent": json.load(open("secrets.json", 'r'))["USER_AGENT"]
     }
     try:
         response = requests.get(url, headers=headers)
         if response.status_code == 200:
-            print("We did it")
             response = response.json()
-            print(response)
             return response
         else:
             print("Error: ", response.status_code)
