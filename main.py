40289e68b83a84dde2312382ded4416032e52076
jonathanoyen0 <149101224+jonathanoyen0@users.noreply.github.com>
2025-03-24 23:59:14 -0500

    Switched to a better implimentation of the 'get_oath_token()' function

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 import requests
 import pandas as pd
+import json
 
-#Uses a file named 'pw.txt' to store the values 
+#Uses a file named 'pw.json' and to store the values 
 def get_oath_token():
-    CLIENT_ID = ""
-    CLIENT_SECRET = ""
-    USER_AGENT = ""
-    
+    secrets = json.load(open("secrets.json"))
+    auth = requests.auth.HTTPBasicAuth(secrets["CLIENT_ID"], secrets["CLIENT_SECRET"])
+    data = json.load(open("pw.json", 'r'))
+    headers = {"User-agent":"API demmo/1.0 by jonathanoyen0"}
+    try:
+        response = requests.post(    'https://www.reddit.com/api/v1/access_token', auth=auth, data=data, headers=headers)
+        if(response.status_code == 200):
+            print("Got oath token")
+            response = response.json()
+            return response
+        else:
+            print("Error: ", response.status_code)
+            return None
+    except requests.exceptions.RequestException as e:
+        print("Error: ", e)
+        return None
+
 
 def get_random_post(subreddit: str):
     url = f"https://www.reddit.com/r/{subreddit}/new.json"
@@ -23,3 +37,5 @@ def get_random_post(subreddit: str):
         print("Error: ", e)
         return None
 
+
+get_oath_token()
