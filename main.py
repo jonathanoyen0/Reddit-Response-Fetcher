6dd12ce0b66c34870148ae2d1e554c004dddab88
jonathanoyen0 <149101224+jonathanoyen0@users.noreply.github.com>
2025-03-24 22:49:50 -0500

    Deleted get oath method, transitioning to a different approach to store sensitive info

--- main.py
+++ main.py
@@ -1,38 +1,5 @@
 import requests
 import pandas as pd
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-def get_oauth_token():
-    CLIENT_ID = os.environ.get("CLIENT_ID")  # Stored in environment variables
-    CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
-    USER_AGENT = os.environ.get("USER_AGENT")
-    PASSWORD = os.environ.get("PASSWORD")
-    USERNAME = os.environ.get("USERNAME")
-
-    auth = requests.auth.HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET)
-
-    data = {'grant_type': 'password',
-            'password': PASSWORD,
-            'username' : USERNAME}
-    
-    headers = {'User-Agent': USER_AGENT}
-
-    response = requests.post("https://www.reddit.com/api/v1/access_token",
-                             auth=auth, data=data, headers=headers)
-    try:
-        if response.status_code == 200:
-            token = response.json().get('access_token')
-            print("OAuth Token:", token)  # Debugging, remove in production
-            return token
-        else:
-            print("Error obtaining token:", response.status_code)
-            return None
-    except requests.exceptions.RequestException as e:
-        print("Error: ", e)
-        return None
 
 
 def get_random_post(subreddit: str):
@@ -50,10 +17,3 @@ def get_random_post(subreddit: str):
         print("Error: ", e)
         return None
 
-
-# df = pd.read_csv("subreddits_public.csv",names=["base10_id","base36_reddit_id","creation_epoch","subreddit_name","subscribers_count"])
-
-
-
-
-# print(get_oauth_token())
