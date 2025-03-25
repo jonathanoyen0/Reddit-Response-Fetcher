d41fd23bdf0e353356940c072fe25cb299f9276a
jonathanoyen0 <149101224+jonathanoyen0@users.noreply.github.com>
2025-03-24 22:46:21 -0500

    did some things

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import requests
 import pandas as pd
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_oauth_token():
-    CLIENT_ID = os.getenv("CLIENT_ID")  # Stored in environment variables
-    CLIENT_SECRET = os.getenv("CLIENT_SECRET")
-    USER_AGENT = os.getenv("USER_AGENT")
+    CLIENT_ID = os.environ.get("CLIENT_ID")  # Stored in environment variables
+    CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
+    USER_AGENT = os.environ.get("USER_AGENT")
+    PASSWORD = os.environ.get("PASSWORD")
+    USERNAME = os.environ.get("USERNAME")
 
     auth = requests.auth.HTTPBasicAuth(CLIENT_ID, CLIENT_SECRET)
-    data = {'grant_type': 'client_credentials'}
+
+    data = {'grant_type': 'password',
+            'password': PASSWORD,
+            'username' : USERNAME}
+    
     headers = {'User-Agent': USER_AGENT}
 
     response = requests.post("https://www.reddit.com/api/v1/access_token",
@@ -42,5 +51,9 @@ def get_random_post(subreddit: str):
         return None
 
 
-df = pd.read_csv("subreddits_public.csv",names=["base10_id","base36_reddit_id","creation_epoch","subreddit_name","subscribers_count"])
+# df = pd.read_csv("subreddits_public.csv",names=["base10_id","base36_reddit_id","creation_epoch","subreddit_name","subscribers_count"])
 
+
+
+
+# print(get_oauth_token())
