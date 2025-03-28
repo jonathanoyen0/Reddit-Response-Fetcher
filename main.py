f76432a0f27d3c05f93397685b3ca29cc7b3c61c
jonathanoyen0 <149101224+jonathanoyen0@users.noreply.github.com>
2025-03-28 14:11:09 -0500

    Final solution

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-from random_post import get_random_post, get_id_post
+from random_post import get_random_post
 from oauth import get_oauth_token
-import random 
+import random
+import json
 
 get_oauth_token()
-print(get_random_post())
+post = get_random_post().json()
+id = post["data"]["after"]
+
+for i in range(1):
+    post = get_random_post(id=id).json()
+    id = post["data"]["after"]
+
+
+
--- random_post.py
+++ random_post.py
@@ -2,18 +2,19 @@ import requests
 import json
 
 #returns the newest reddit post on a given subreddit, returns response from api in json
-def get_random_post(id = "None"):
+def get_random_post(id = None):
     headers = {
         "Authorization": json.load(open("oath_token.json", 'r'))["token"],
         "User-Agent": json.load(open("secrets.json", 'r'))["USER_AGENT"]
     }
     try:
-        if id != "None":
-            response = requests.get(f"https://www.reddit.com/r/all/new.json?afer={id}", headers=headers)
+        if id:
+            response = requests.get(f"https://oauth.reddit.com/r/all/new.json?after={id}", headers=headers)
         else:
-            response = requests.get("https://www.reddit.com/all/new.josn", headers=headers)
+            response = requests.get("https://oauth.reddit.com/r/all/new.json", headers=headers)
         if response.status_code == 200:
-            return response.json()
+            print("got post")
+            return response
         else:
             print("Error: ", response.status_code)
             return None
